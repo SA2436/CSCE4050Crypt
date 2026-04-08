5b9785dba940f42ced650871cb056f8b4a60bb37
SA2436 <stephenalonso@my.unt.edu>
2026-04-08 10:06:46 -0500

    Clean up Task 2
    server2.py and client2.py cleaned up to show for each step on Task 2

--- client2.py
+++ client2.py
@@ -8,51 +8,65 @@ import hashlib
 
 #Task 2 Step 1 - 8 Hardcode a pre-shared secret key same from the server
 secretKey = b'28282828282828282828282828282828' #Using the same method 32-byte key from Homework 5 using the last two student ID 
-
-mac_key = b'cadc3239e73e53622d0664dff2651767f618a438f00ede9a6541fb1cb4835b05' #Hardcoded for ease input from tag.txt from previce steps on task 2
+#FOR Task 2.6
+#mac_key = b'cadc3239e73e53622d0664dff2651767f618a438f00ede9a6541fb1cb4835b05' #Hardcoded for ease input from tag.txt from previce steps on task 2
 
 url = "http://127.0.0.1:5028/weather"  # Add the last two digits with Stephen Alonso's student ID (Edit)
-#url = "http://127.0.0.1:5029/weather" # For task 2.4 to connect to server_fake
+
+#For task 2.4 to connect to server_fake
+#url = "http://127.0.0.1:5029/weather" 
 
 res = requests.get(url) #Send GET request to the weather (edited with the help from ChatGPT to GET the weather data)
-print("Raw response: ") #Fix to see what on the server2.py is bugging (ChatGPT)
-print(res.text)
+#IF errors when running client#.py
+#print("Raw response: ") #Fix to see what on the server2.py is bugging (From ChatGPT to view the response when running code)
+#print(res.text)
 
 response = res.json()
 
-nonce = base64.b64decode(response['nonce'])
-ciphertext = base64.b64decode(response['ciphertext'])
-tag = base64.b64decode(response['tag'])
-
-#Verify new computed MAC
-data_mac = nonce + ciphertext
-new_mac = hmac.new(mac_key, data_mac, hashlib.sha256).digest()
-
-#Test prints the tags should not match
-if new_mac != tag:
-    print("Failed integrity test")
-    print("Client will not be able to access data to mismatch data")
-    exit()
-
-#Decryption for Task 2.6 from task 2.2
-cipher = AES.new(secretKey, AES.MODE_CTR, nonce=nonce)
-plaintext = cipher.decrypt(ciphertext)
-
-#Print out authenticated server
-print("Authentication Successful")
-print(json.loads(plaintext)) #ChatGPT fix to printing out the authenticated server
-
-#Task 2.2Fix from ChatGPT to decoding the base64 value from server2 also got rid of the if and else statement due to errors
-#nonce = base64.b64decode(response['nonce'])
-#ciphertext = base64.b64decode(response['ciphertext'])
-#Task 2.2 Initailize and store the AES-CTR cipher from server to then decrypt and display weather data
-#cipher = AES.new(secretKey, AES.MODE_CTR, nonce=nonce)
-#plaintext = cipher.decrypt(ciphertext)
-#print("Decrypted message:") #Print out the indicated decrypted message
-#print(json.loads(plaintext)) #Print Successful getting weather info from server
-
-#Task 2.3
+#Task 2.3 FOUR LINES BELOW:
 #data = response['data']
 #tag = response['tag']
 #ciphertext = json.dumps(data, sort_keys=True).encode() #Fix with ChatGPT from mismatch data and tag
 #decode_tag = hmac.new(secretKey, ciphertext, hashlib.sha256).hexdigest()
+
+#FOR Task 2.2 & 2.6 Fix from ChatGPT to decoding the base64 value from server2 also got rid of the if and else statement due to errors
+nonce = base64.b64decode(response['nonce'])
+ciphertext = base64.b64decode(response['ciphertext'])
+
+#FOR Task 2.6
+#tag = base64.b64decode(response['tag'])
+
+#FOR Task 2.6 Verify new computed MAC
+#data_mac = nonce + ciphertext
+#new_mac = hmac.new(mac_key, data_mac, hashlib.sha256).digest()
+
+#FOR Task 2.6 Test prints the tags should not match
+#if new_mac != tag:
+    #print("Failed integrity test")
+    #print("Client will not be able to access data to mismatch data, Exiting Now")
+    #exit()
+
+#FOR Task 2.2 & Task 2.6 Decryption 
+cipher = AES.new(secretKey, AES.MODE_CTR, nonce=nonce)
+plaintext = cipher.decrypt(ciphertext)
+
+#FOR Task 2.2 Initailize and store the AES-CTR cipher from server to then decrypt and display weather data
+print("Decrypted message:") #Print out the indicated decrypted message
+print(json.loads(plaintext)) #Print Successful getting weather info from server
+
+#FOR Task 2.3 with if data was tampered or not tampered to match HMAC tag from the server's HMAC tag
+#if decode_tag = tag:
+    #print("Data authenticated with HMAC tag")
+    #print(data)
+#else:
+    #print("Data has been tampered")
+
+#FOR Task 2.6 Print out authenticated server TWO LINE BELOW:
+#print("Authentication Successful")
+#print(json.loads(plaintext)) #ChatGPT fix to printing out the authenticated server
+
+
+
+
+
+
--- server2.py
+++ server2.py
@@ -10,14 +10,17 @@ import base64, json
 import hmac 
 import hashlib
 
+#TODO for different steps in Task 2 REMOVE (#) under the appropriate task to run
+
+#Task 1-4 for flask run
 app = Flask(__name__)
 
 #Task 2 Step 1 - 8 Hardcode a pre-shared secret key 
 secretKey = b'28282828282828282828282828282828' #Using the same method 32-byte key from Homework 5 using the last two student ID 
-#Task 2.
-mac_key = b'cadc3239e73e53622d0664dff2651767f618a438f00ede9a6541fb1cb4835b05' #Hardcoded for ease input from tag.txt from previce steps on task 2
+#FOR: Task 2.8 
+#mac_key = b'cadc3239e73e53622d0664dff2651767f618a438f00ede9a6541fb1cb4835b05' #Hardcoded for ease input from tag.txt from previce steps on task 2
 
-#Weather endpoint returning a static JSON payload with weather Denton data
+#Task 1-4 Weather endpoint returning a static JSON payload with weather Denton data 
 @app.route('/weather', methods=['GET'])
 def weather():
     data = {
@@ -28,36 +31,47 @@ def weather():
         "humidity_percent": 28  #Add with Stephen Alonso's last two of Student ID # digits (28) (Edit)
     }
     #Task 2 Step 2 and 6 #Encrypt PT with AES-CTR to only provide confidentiality
-    plaintext = json.dumps(data).encode() #Fix from ChatGPT to converting python dictionary to JSON string to bytes instead of doing bytes.fromhex
+    plaintext = json.dumps(data).encode() #Fix with ChatGPT to converting python dictionary to JSON string to bytes instead of doing bytes.fromhex
+    #Use the line below if running into errors for steps 2-6
+    #plaintext = json.dumps(data, sort_keys=True).encode()
+
+    #Task 2.2 
     cipher = AES.new(secretKey, AES.MODE_CTR) #AES-CTR from homework 5 with the secret key and generate random nonce 
     ciphertext = cipher.encrypt(plaintext) #AES-CTR encryption PT to CT
 
-    #Task 2.6 MAC encrypted with nonce and computed ciphertext
-    data_mac = cipher.nonce + ciphertext
-    tag = hmac.new(mac_key, data_mac, hashlib.sha256).digest()#Task 2.6 new computed mac key to push to response.bin (FIX with ChatGPT)
+    #FOR Task 2 step 3 and 6 Compute HMAC-SHA256 tag
+    #tag = hmac.new(secretKey, plaintext, hashlib.sha256).hexdigest()
+    #Save HMAC-SHA256 tag into tag.txt for later project use
+    # with open("tag.txt", "w") as f:
+    #    f.write(tag)
+
+    #FOR Task 2.6 MAC encrypted with nonce and computed ciphertext (Encrypt-then-MAC)
+    #data_mac = cipher.nonce + ciphertext
+    #FOR Task 2.6 MAC encrypted with nonce and computed ciphertext
+    #tag = hmac.new(mac_key, data_mac, hashlib.sha256).digest()#Task 2.6 new computed mac key to push to response.bin (FIX with ChatGPT)
 
     enc_data = {
         #Encoding all the binary data to Base64 (advice from GeeksfrGeeks.org and ChatGPT) instead of ChaCha20 
         "nonce": base64.b64encode(cipher.nonce).decode(),
         "ciphertext": base64.b64encode(ciphertext).decode(),
-        "tag": base64.b64encode(tag).decode()
+        #FOR Task 2.3
+        #"tag": base64.b64encode(tag).decode()
     }
     #Task 2.6 records server's response
-    with open("response.bin", "wb") as f:
-        f.write(cipher.nonce + tag + ciphertext)
+    #with open("response.bin", "wb") as f:
+        #f.write(cipher.nonce + tag + ciphertext)
 
-    return jsonify(enc_data)
+    #FOR Task2.3 ONLY
+    #return jsonify({ "data": data, "tag": tag })
+    #For every other Task 2 steps
+    return jsonify(enc_data) #send JSON response to client when requested 
 
     #From previous task steps
-    #return jsonify(enc_data) #send JSON response to client when requested 
+    #return jsonify(enc_data) 
     
-    #plaintext = json.dumps(data, sort_keys=True).encode()
+    
 
-    #Task 2 step 3 and 6Compute HMAC-SHA256 tag
-    #tag = hmac.new(secretKey, plaintext, hashlib.sha256).hexdigest()
-    #Save HMAC-SHA256 tag into tag.txt for later project use
-    # with open("tag.txt", "w") as f:
-    #    f.write(tag)
+    
 
     #return jsonify({"data": data, "tag": tag}) #send JSON response to client when requested 
 
